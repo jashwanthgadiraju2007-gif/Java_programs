ecc0a9b686f0efb2a1f8f3d6b20682cd5cc20d4f
Jashwanth G <your_github_email@gmail.com>
2026-02-12 21:04:00 +0530

    Intial commit

---
+++ src/number_programs/Happy_number.java
@@ -0,0 +1,26 @@
+package number_programs;
+
+public class Happy_number{
+	public static void code(int num) {
+		int temp = num;
+		while(num!= 1 && num!= 4) {
+			int sum=0;
+			while(num>0) {
+				int rem = num %10 ;
+				sum = sum + (rem*rem);
+				num/=10;
+			}
+			num = sum;
+		}
+		if(num==1) {
+			System.out.println(temp+" is a happy number");
+		}
+		System.out.println(temp+" is not a happy number");
+		
+	}
+public static void main(String[] args) {
+	code(81);
+	
+	
+}
+}
