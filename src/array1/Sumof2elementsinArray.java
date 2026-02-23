4808e5af5097ba77d4925111e9d30980e7ce73d0
Jashwanth G <jashwanthgadiraju2007@gmail.com>
2026-02-23 20:43:22 +0530

    sum of 2 elemnts in array to get target

--- src/array1/Sumof2elementsinArray.java
+++ src/array1/Sumof2elementsinArray.java
@@ -1,27 +1,29 @@
 package array1;
 
 public class Sumof2elementsinArray {
-public static int[] Sum(int ar1[],int num) {
+public static void Sum(int ar1[],int num) {
 	int res[]=new int[2];
 	for(int i = 0;i<ar1.length;i++) {
-		for(int j = 0 ;j<ar1.length;j++) {
+		for(int j = i+ 1;j<ar1.length;j++) {
+			if(i < j) {
 			if(num == ar1[i] +ar1[j]) {
-				res[0] = j;
-				res[1] = i ;
+				res[0] = ar1[j];
+				res[1] = ar1[i] ;
+			System.out.println("two numbers are"+res[0]+"and"+res[1]);
 			}
 			
 		}
 	}
-		return res;
+	}
+	
 	
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int ar1[]= {2,4,6,8,10};
-		int result[] = Sum(ar1, 12);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
+		int ar1[]= {2,4,6,8,6,10};
+   
+          Sum(ar1,12);
+          
           }
 
-}
 }
