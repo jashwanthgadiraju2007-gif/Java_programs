a05f5428c43bd2592cf94e9615de69bb116d28f7
Jashwanth G <jashwanthgadiraju2007@gmail.com>
2026-02-24 13:44:20 +0530

    sort the array

---
+++ src/array1/Sortarray.java
@@ -0,0 +1,27 @@
+package array1;
+import java.util.Arrays;
+public class Sortarray {
+public static void sort(int a[]) {
+	 for (int i = 0; i < a.length - 1; i++) {
+         for (int j = i + 1; j < a.length; j++) {
+             if (a[i] > a[j]) {
+                 int temp = a[i];
+                 a[i] = a[j];
+                 a[j] = temp;
+             }}
+     }
+	 System.out.println(Arrays.toString(a));
+
+//	for(int k= 0;k<a.length;k++) {
+//	System.out.println(a[k]);
+//	}
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {8,4,3,5,6};
+		sort(a);
+
+	}
+
+}
